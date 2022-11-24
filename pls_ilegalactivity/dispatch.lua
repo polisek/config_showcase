0aa87443b51bb45a5ed4be321990b88f78d9ccdc
polisek <107623238+polisek@users.noreply.github.com>
2022-11-24 09:27:27 +0100

    Update dispatch.lua

--- pls_ilegalactivity/dispatch.lua
+++ pls_ilegalactivity/dispatch.lua
@@ -2,6 +2,28 @@ RegisterNetEvent("pls_ilegalactivity:client:signalToPolice")
 AddEventHandler("pls_ilegalactivity:client:signalToPolice",function(whatsignal)
     if whatsignal == Config.PoliceSignalDefinition.rob_atm_rob then
         -- Add your dispatch code
+        -- Example
+        /*
+            Example
+            local data = exports['cd_dispatch']:GetPlayerInfo()
+            TriggerServerEvent('cd_dispatch:AddNotification', {
+                job_table = Config.PoliceJobs, 
+                coords = data.coords,
+                title = '10-XX - Scrap car',
+                message = 'A '..data.sex..' steals a car '..data.street, 
+                flash = 0,
+                unique_id = tostring(math.random(0000000,9999999)),
+                blip = {
+                    sprite = 431, 
+                    scale = 1.2, 
+                    colour = 3,
+                    flashes = false, 
+                    text = '10-XX - Scrap car',
+                    time = (5*60*1000),
+                    sound = 1,
+                }
+            })
+        */
         print("Dispatch call "..whatsignal)
     elseif whatsignal == Config.PoliceSignalDefinition.rob_gummachine then
         -- Add your dispatch code
